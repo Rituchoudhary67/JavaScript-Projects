2d97c409d37c32167aabadaf57762bf0ccdf6c83
Rituchoudhary67 <ritulalitchoudhary@gmail.com>
2023-03-03 13:35:20 +0530

    working on js part

---
+++ index.js
@@ -0,0 +1,14 @@
+const refreshBtn = document.querySelector(".ref-btn");
+// console.log(refreshBtn)
+
+// step2 makeing that function 
+function randomColor() {
+//   console.log("hello")
+
+}
+
+
+
+
+// step 1 add event listener to btn
+refreshBtn.addEventListener("click",randomColor);
