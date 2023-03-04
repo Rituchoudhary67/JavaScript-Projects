984d47fefcb4d3051acd2d481d6af5b0af9250e4
Rituchoudhary67 <ritulalitchoudhary@gmail.com>
2023-03-05 00:25:38 +0530

    done

--- index.js
+++ index.js
@@ -1,15 +1,28 @@
 const refreshBtn = document.querySelector(".ref-btn");
+const container = document.querySelector(".container")
+let maxNoOfBox = 10;
 // console.log(refreshBtn)
 
 // step2 makeing that function 
 function randomColor() {
-//   console.log("hello")
-let randomNo = Math.floor(Math.random() * 0xffffff).toString(16);
-// randomNo = `#{$randomNo.pad}`
-console.log(randomNo)
 
-}
+    // clearing the container
+    container.innerHTML = "";
+    for(let i=0; i< maxNoOfBox; i++){
+    //generating random no
+    let randomNo = Math.floor(Math.random() * 0xffffff).toString(16);
+    randomNo = `#${randomNo.padStart(6,"0")}`
 
+    const color = document.createElement("li");
+    color.classList.add("color");
+    color.innerHTML = `
+    <div class="react-box" style="background:${randomNo}"></div>
+    <span class="hex-value">${randomNo}</span>`;
+
+    container.appendChild(color)
+    }
+ }
+randomColor();
 
 
 
